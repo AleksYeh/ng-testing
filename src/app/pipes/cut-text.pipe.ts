22896b7b0a56e12d0d9a76f6b2f794272a2de71b
Твоё Имя <dorovskoy567@mail.ru>
2026-08-10 15:40:19 +0300

    Обновить компонент CutTextPipe и изменить шаблон приложения для отображения обрезанного текста

--- src/app/pipes/cut-text.pipe.ts
+++ src/app/pipes/cut-text.pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CutTextPipe implements PipeTransform {
 
   transform(value: string, maxLength=100): string {
-    if(value.length > maxLength) return value.substring
-    return null;
+    if(value.length > maxLength) return value.substring(0, maxLength)+'...'
+    else return value;
   }
 
 }
