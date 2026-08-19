ee4d4912be8ea6f3a9b016b1443409c0a5b40d5d
Твоё Имя <dorovskoy567@mail.ru>
2026-08-19 15:00:30 +0300

    feat: добавить объект с данными и метод для отслеживания элементов в массиве

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,21 @@ export class AppComponent {
   str: string = '';
 
   arr: any[] = ['text', 123, true, [1, 'abc'], NaN, Symbol];
+
+  obj: any = {
+    first: {
+      second: {
+        third: {
+          age: 11,
+          name: 'Mike',
+        },
+      },
+    },
+  };
+
+  someTrackByMethod(index: number, item: any) {
+    console.log(1);
+
+    return item;
+  }
 }
