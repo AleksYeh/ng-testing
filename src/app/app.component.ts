aa071667cd3703fb59fd6c79b46abf7dc67a07c4
Твоё Имя <dorovskoy567@mail.ru>
2026-08-19 15:45:42 +0300

    feat: обновить разметку и стили компонента, удалив неиспользуемые элементы и улучшив структуру

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  toggler: boolean = true;
-  str: string = '';
+  color: string = 'orange';
 
-  arr: any[] = ['text', 123, true, [1, 'abc'], NaN, Symbol];
-
-  obj: any = {
-    first: {
-      second: {
-        third: {
-          age: 11,
-          name: 'Mike',
-        },
-      },
-    },
+  cssProperties = {
+    color: 'orange',
+    background: 'green',
   };
-
-  someTrackByMethod(index: number, item: any) {
-    console.log(1);
-
-    return item;
-  }
 }
