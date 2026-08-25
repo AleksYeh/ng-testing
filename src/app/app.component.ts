2a1ec86767ffca79c2f59dc9d3dcdefdd72e5f68
Твоё Имя <dorovskoy567@mail.ru>
2026-08-25 17:54:10 +0300

    feat: добавить условный рендеринг в родительском компоненте и обновить дочерний компонент с новыми входными данными

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,45 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Заголовок';
   name: string = 'Имя';
+  toggler: boolean = true;
+
+  obj = { age: 12 };
 
   constructor() {
-    setTimeout(() => {
-      this.title = 'Другой заголовок';
-    }, 3000);
+    // setTimeout(() => {
+    //   this.title = 'Другой заголовок';
+    //   // this.obj.age = 10;
+    //   this.obj = { ...this.obj, age: 10 };
+    // }, 3000);
+    //   console.log(
+    //     '%c Компонент app-root успешно создан!',
+    //     'background: darkgreen'
+    //   );
+    // }
+    // ngOnChanges(changes: SimpleChanges): void {
+    //   console.log('%c родительский ngOnchanges', 'color: aqua');
+    //   // console.log('changes', changes);
+    // }
+    // ngOnInit(): void {
+    //   console.log('%c родительский ngOninit', 'color: deepskyblue');
+    // }
+    // ngDoCheck(): void {
+    //   console.log('%c родительский ngDocheck', 'color: pink');
+    // }
+    // ngAfterContentInit(): void {
+    //   console.log('%c родительский ngArfetContentInit', 'color: lightgreen');
+    // }
+    // ngAfterContentChecked(): void {
+    //   console.log('%c родительский ngArfetContentChecked', 'color: green');
+    // }
+    // ngAfterViewInit(): void {
+    //   console.log('%c родительский ngArfetViewInit', 'color: yellow');
+    //   setTimeout(() => console.log('title внутри  setTimeout'));
+    // }
+    // ngAfterViewChecked(): void {
+    //   console.log('%c родительский ngArfetViewChecked', 'color: orange');
+    // }
+    // ngOnDestroy(): void {
+    //   console.log('%c ngOnDestroy', 'color: red');
   }
 }
