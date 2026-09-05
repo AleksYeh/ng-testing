6f7350f768671790c18327d2cf4df59c627d10f5
Твоё Имя <dorovskoy567@mail.ru>
2026-09-05 18:37:30 +0300

    feat: обновить компоненты и директивы, добавив динамическое создание компонентов и улучшив структуру

--- src/app/directives/custom-directive.directive.ts
+++ src/app/directives/custom-directive.directive.ts
@@ -1,32 +1,13 @@
 import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { DynamicComponent } from '../components/dynamic/dynamic.component';
 
 @Directive({
   selector: '[appCustomDirective]',
   exportAs: 'custom',
 })
 export class CustomDirectiveDirective {
-  @Input() appCustomDirective: any;
-  @Input() appCustomDirectiveHello: any;
-
-  time!: Date;
-
-  constructor(
-    private template: TemplateRef<any>,
-    private viewContainer: ViewContainerRef
-  ) {}
-
-  // ngOnInit() {
-  //   this.viewContainer.createEmbeddedView(this.template);
-  // }
-
-  ngOnChanges() {
-    console.log('hello', this.appCustomDirectiveHello);
-
-    this.time = new Date();
-    this.appCustomDirective
-      ? this.viewContainer.createEmbeddedView(this.template, {
-          time: this.time,
-        })
-      : this.viewContainer.clear();
+  constructor(private viewContainer: ViewContainerRef) {}
+  ngOnInit() {
+    this.viewContainer.createComponent(DynamicComponent);
   }
 }
