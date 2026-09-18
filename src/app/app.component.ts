59a67f6f293ce0a544d84680540f88ffa816c012
Твоё Имя <dorovskoy567@mail.ru>
2026-09-18 16:00:40 +0300

    feat: обновить компоненты и стили, улучшив структуру и добавив новые файлы стилей

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,9 @@
-import { Component, ViewChild, ViewContainerRef } from '@angular/core';
-// import { DynamicComponent } from './components/dynamic/dynamic.component';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
-export class AppComponent {
-  toggler: boolean = true;
-
-  @ViewChild('example', { read: ViewContainerRef })
-  container!: ViewContainerRef;
-
-  constructor(private viewContainer: ViewContainerRef) {}
-
-  showComponent() {
-    // this.viewContainer.createComponent(DynamicComponent);
-  }
-
-  async asyncShowComponent() {
-    const { DynamicComponent } =
-      await import('./components/dynamic/dynamic.component');
-    this.viewContainer.createComponent(DynamicComponent);
-  }
-
-  ngAfterViewInit() {
-    // const newComponent = this.container.createComponent(DynamicComponent);
-    // newComponent.instance.name = 'new name';
-    // newComponent.instance.callName();
-    // newComponent.setInput('name', 'new name');
-    // const someData = newComponent.instance.callName();
-    // console.log('someData', someData);
-  }
-}
+export class AppComponent {}
--- src/app/shared/components/green/green.component.ts
+++ src/app/shared/components/green/green.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { RedComponent } from '../red/red.component';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-green',
   templateUrl: './green.component.html',
   styleUrl: './green.component.scss',
   standalone: true,
-  imports: [RedComponent],
+  // imports: [RedComponent],
+  encapsulation: ViewEncapsulation.None,
 })
 export class GreenComponent {}
